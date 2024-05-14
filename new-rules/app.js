f7b8b48d3a2e8bee93dd3cc9987a3fbd8b313e18
bdelorean <9021196@student.zadkine.nl>
2024-05-14 10:00:32 +0200

    theory

---
+++ new-rules/app.js
@@ -0,0 +1,24 @@
+//normal function
+function changeName(name){
+    return "Hallo mijn naam is" + name
+    `Hallo mijn naam is ${name}`
+}
+console.log(Bianca);
+//arrow function
+const changeName = (name) => {
+return 'Hallo mijn naam is '+ name;
+};
+//parameters in een functie is om  van buitenaf informatie in de functie te geven 
+ //maak een array van auto's met 3 auto merken
+
+ //for loop
+ let cars = ['Bmw','Audi','Vw']
+
+ for(let i=0; i < cars.length; i++){
+    console.log(autos[i]);
+ }
+ //for-of loop
+  for(let car of cars){
+console.log(car);
+// elke item van de array krijg tijdelijk elke keer 'car' zo laat je elke ronde maar 1 element zien 
+  }
---
+++ new-rules/new-rules.md
@@ -0,0 +1,32 @@
+# New Rules JavaScript
+## Functions
+
+```
+//normal function
+function changeName{
+    
+}
+//arrow function
+const changeName = () => {
+
+}
+```
+## For loop 
+//oude manier
+let cars = ['Bmw','Audi','Vw']
+console.log(cars[1])// hij gaat de Audi laten zien
+Om alle elementen onder elkaar te laten zien moet een for loop gebruiken 
+```
+for(let i=0; i < cars.length; i++){
+    console.log(autos[i]);
+ }
+
+```
+### for-of loop 
+//nieuwe manier
+```
+for(let car of cars){
+    console.log(car)
+}
+``````
+// elke element van de array krijgt tijdelijk de naam 'car' dus hij gaat elke rond een element van de array laten zien //
