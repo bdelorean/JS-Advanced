313c30ca4248c91b52b3a80df5b7ba18292e08bd
Jason <83275145+jschaik@users.noreply.github.com>
2024-05-20 10:12:14 +0200

    feedback product opdracht

--- objects/product.js
+++ objects/product.js
@@ -22,3 +22,17 @@ function createImg() {
 `;
 }
 createImg();
+
+//je hebt een beetje onhandig gedaan met de afbeelding ik zal je laten zien hoe het makkelijker gaat.
+//je hebt de functie createImg ook niet nodig.
+
+displayProduct.innerHTML += `
+  <h1>${bag.name}</h1>
+  <div>
+    <p>${bag.color}</p>
+    <p>${bag.type}</p>
+    <p>${bag.price}</p>
+    //dit is het enige ;)
+    <img src="${bag.img}"> 
+  </div>
+  `;
